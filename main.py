9d3ec6c57fccf0b8c4d1068b8250a8854dfd4f13
ezschneider <ezequielschneiderr@gmail.com>
2022-06-14 18:38:05 -0300

    KMC class

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def classification_report(scores):
 # Importando a base de dados Iris
 from sklearn import datasets
 
-iris = datasets.load_iris()
-iris_x = iris.data
-iris_y = iris.target   
+data = datasets.load_breast_cancer()
+data_X = data.data
+data_y = data.target   
 
 # Parte I
 # Importando os Classificadores ZeroR e Naive Bayes Gaussiano
@@ -35,19 +35,40 @@ gNB = GaussianNB()
 pipeline_gNB = Pipeline([('transformer', scalar), ('estimator', gNB)])
 
 print('ZR:')
-classification_report(cross_val_score(pipeline_zR, iris_x, iris_y, scoring='accuracy', cv=rkf))
+classification_report(cross_val_score(pipeline_zR, data_X, data_y, scoring='accuracy', cv=rkf))
 print('GNB:')
-classification_report(cross_val_score(pipeline_gNB, iris_x, iris_y, scoring='accuracy', cv=rkf))
+classification_report(cross_val_score(pipeline_gNB, data_X, data_y, scoring='accuracy', cv=rkf))
 
 # Parte II
 # Importando os Classificadores KNN, KMC e AD
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-#from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans
+from sklearn.utils.validation import check_X_y
+
+class KMeansClassifier():
+    def __init__(self, cx=None, cy=None):
+        super().__init__()
+        self.cx = cx
+        self.cy = cy
+
+    def fit(self,x_train,y_train):
+        x_train,y_train = check_X_y(x_train,y_train)
+
+        counter = KMeans()
+        self.__self_pred = max(counter,key = counter.get)
+
+    def predict(self,x_test):
+        (n,_) = x_test.shape
+        n0 = floor(n*self.cx/100)
+        n1 = floor(n*self.cy/100)
+        nprd = n - (n0 + n1)        
+        prd = [self.__self_pred]*nprd + [0]*n0 + [1]*n1
+        return np.array(prd)
 
 # Definindo os hiperparametros
 parameters_KNN = {'estimator__n_neighbors':[1,3,5,7]}
-parameters_AD = {'estimator__max_deph':[None,3,5,10]}
+parameters_AD = {'estimator__max_depth':[None,3,5,10]}
 parameters_KMC = {'estimator__k':[1,3,5,7]}
 
 kNN = KNeighborsClassifier()
@@ -58,14 +79,13 @@ aD = DecisionTreeClassifier()
 pipeline_AD = Pipeline([('transformer', scalar), ('estimator', aD)])
 p_AD = GridSearchCV(pipeline_AD, parameters_AD, scoring='accuracy', cv=4)
 
-# Implementar o KMC
-#kMC = KMeans()
-#pipeline_kMC = Pipeline([('transformer', scalar), ('estimator', kMC)])
-#p_KMC = GridSearchCV(pipeline_kMC, parameters_KMC, scoring='accuracy', cv=4)
+kMC = KMeansClassifier()
+pipeline_kMC = Pipeline([('transformer', scalar), ('estimator', kMC)])
+p_KMC = GridSearchCV(pipeline_kMC, parameters_KMC, scoring='accuracy', cv=4)
 
 print('KNN:')
-classification_report(cross_val_score(p_KNN, iris_x, iris_y, scoring='accuracy', cv=rkf))
+classification_report(cross_val_score(p_KNN, data_X, data_y, scoring='accuracy', cv=rkf))
 print('AD:')
-classification_report(cross_val_score(p_AD, iris_x, iris_y, scoring='accuracy', cv=rkf))
+classification_report(cross_val_score(p_AD, data_X, data_y, scoring='accuracy', cv=rkf))
 #print('KMC:')
-#classification_report(cross_val_score(p_KMC, iris_x, iris_y, scoring='accuracy', cv = rkf))
+#classification_report(cross_val_score(p_KMC, data_X, data_y, scoring='accuracy', cv = rkf))
