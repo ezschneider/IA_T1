619d618866166b6df6b6daa8363d4cd8138bd506
Ezequiel Schneider <ezequielschneiderr@gmail.com>
2022-06-20 13:58:56 -0300

    Table scores

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from scipy import stats
+import pandas as pd
 import numpy as np
+
+table_result = pd.DataFrame(columns=['Método', 'Média', 'Desvio Padrão', 'Limite Inferior', 'Limite Superior'])
 
 # Função para mostrar as medidas alvos
 def classification_report(scores):
@@ -12,6 +15,17 @@ def classification_report(scores):
     inf, sup = stats.norm.interval(0.95, loc=scores.mean(), 
                                scale=scores.std()/np.sqrt(len(scores)))
     print(f'Intervalo de confiança (95%): [{inf:.2f},{sup:.2f}]')
+
+# Função para adicionar as medidas alvos na tabela de scores
+def add_result(met, df, scores):
+    inf, sup = stats.norm.interval(0.95, loc=scores.mean(), 
+                               scale=scores.std()/np.sqrt(len(scores)))
+    new_row = pd.DataFrame([[met, round(scores.mean(), 2), round(scores.std(), 2), round(inf, 2), round(sup, 2)]],
+                            columns=['Método', 'Média', 'Desvio Padrão', 'Limite Inferior', 'Limite Superior'])
+
+    df = pd.concat([df, new_row])
+    return df
+    
 
 # Importando a base de dados Iris
 from sklearn import datasets
@@ -34,10 +48,10 @@ pipeline_zR = Pipeline([('transformer', scalar), ('estimator', zR)])
 gNB = GaussianNB()
 pipeline_gNB = Pipeline([('transformer', scalar), ('estimator', gNB)])
 
-print('ZR:')
-classification_report(cross_val_score(pipeline_zR, data_X, data_y, scoring='accuracy', cv=rkf))
-print('GNB:')
-classification_report(cross_val_score(pipeline_gNB, data_X, data_y, scoring='accuracy', cv=rkf))
+table_result = add_result('ZR', table_result, cross_val_score(pipeline_zR, data_X, data_y, scoring='accuracy', cv=rkf))
+#classification_report(cross_val_score(pipeline_zR, data_X, data_y, scoring='accuracy', cv=rkf))
+table_result = add_result('NBG', table_result, cross_val_score(pipeline_gNB, data_X, data_y, scoring='accuracy', cv=rkf))
+#classification_report(cross_val_score(pipeline_gNB, data_X, data_y, scoring='accuracy', cv=rkf))
 
 # Parte II
 # Importando os Classificadores KNN, KMC e AD
@@ -46,30 +60,30 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.cluster import KMeans
 from sklearn.utils.validation import check_X_y
 
-class KMeansClassifier():
-    def __init__(self, cx=None, cy=None):
-        super().__init__()
-        self.cx = cx
-        self.cy = cy
 
-    def fit(self,x_train,y_train):
-        x_train,y_train = check_X_y(x_train,y_train)
+# class KMeansClassifier():
+#    def __init__(self, cx=None, cy=None):
+#        super().__init__()
+#        self.cx = cx
+#        self.cy = cy
 
-        counter = KMeans()
-        self.__self_pred = max(counter,key = counter.get)
+#    def fit(self,x_train,y_train):
+#        x_train,y_train = check_X_y(x_train,y_train)
 
-    def predict(self,x_test):
-        (n,_) = x_test.shape
-        n0 = floor(n*self.cx/100)
-        n1 = floor(n*self.cy/100)
-        nprd = n - (n0 + n1)        
-        prd = [self.__self_pred]*nprd + [0]*n0 + [1]*n1
-        return np.array(prd)
+#        counter = KMeans()
+#        self.__self_pred = max(counter,key = counter.get)
+
+#    def predict(self,x_test):
+       
 
 # Definindo os hiperparametros
 parameters_KNN = {'estimator__n_neighbors':[1,3,5,7]}
 parameters_AD = {'estimator__max_depth':[None,3,5,10]}
-parameters_KMC = {'estimator__k':[1,3,5,7]}
+#parameters_KMC = {'estimator__k':[1,3,5,7]}
+
+#kMC = KMeansClassifier()
+#pipeline_kMC = Pipeline([('transformer', scalar), ('estimator', kMC)])
+#p_KMC = GridSearchCV(pipeline_kMC, parameters_KMC, scoring='accuracy', cv=4)
 
 kNN = KNeighborsClassifier()
 pipeline_kNN = Pipeline([('transformer', scalar), ('estimator', kNN)])
@@ -79,13 +93,11 @@ aD = DecisionTreeClassifier()
 pipeline_AD = Pipeline([('transformer', scalar), ('estimator', aD)])
 p_AD = GridSearchCV(pipeline_AD, parameters_AD, scoring='accuracy', cv=4)
 
-kMC = KMeansClassifier()
-pipeline_kMC = Pipeline([('transformer', scalar), ('estimator', kMC)])
-p_KMC = GridSearchCV(pipeline_kMC, parameters_KMC, scoring='accuracy', cv=4)
-
-print('KNN:')
-classification_report(cross_val_score(p_KNN, data_X, data_y, scoring='accuracy', cv=rkf))
-print('AD:')
-classification_report(cross_val_score(p_AD, data_X, data_y, scoring='accuracy', cv=rkf))
-#print('KMC:')
+#table_result = add_result('KMC', table_result, cross_val_score(p_AD, data_X, data_y, scoring='accuracy', cv=rkf))
 #classification_report(cross_val_score(p_KMC, data_X, data_y, scoring='accuracy', cv = rkf))
+table_result = add_result('KNN', table_result, cross_val_score(p_KNN, data_X, data_y, scoring='accuracy', cv=rkf))
+#classification_report(cross_val_score(p_KNN, data_X, data_y, scoring='accuracy', cv=rkf))
+table_result = add_result('AD', table_result, cross_val_score(p_AD, data_X, data_y, scoring='accuracy', cv=rkf))
+#classification_report(cross_val_score(p_AD, data_X, data_y, scoring='accuracy', cv=rkf))
+
+print(table_result)
